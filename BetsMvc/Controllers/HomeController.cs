035eecc4d92cfc3d98a35c048c7719caf3ed7b3d
Blitz Blitz <raven3001@mail.ru>
2021-09-01 16:46:24 +0300

    Исправлен баг с отображением статистики только для одной команды

--- BetsMvc/Controllers/HomeController.cs
+++ BetsMvc/Controllers/HomeController.cs
@@ -39,8 +39,7 @@ namespace BetsMvc.Controllers
         [HttpPost]
         public ActionResult Filter(int? kills = 0, int? time = 0, int idTeam = 1, int? NumberGame=0)
         {
-            int ResultNumberGame = 0;
-            bool flagNull = false;
+            int ResultNumberGame = 0;            
             List<Map> AllMapsTeam = _myDbContext.Maps.Where(m => m.Game.Teamid == idTeam).ToList();
             List<Game> Allgames = _myDbContext.Games.Where(g=>g.Team.Id==idTeam).ToList();
             List<Game> games = new List<Game>();
@@ -51,10 +50,12 @@ namespace BetsMvc.Controllers
             maps = _myDbContext.Maps.Where(m => m.Kills >= kills &&m.Game.Team.Id==idTeam).ToList();
             if(time!=0 && kills==0)
             maps = _myDbContext.Maps.Where(m => m.Time >= time && m.Game.Team.Id == idTeam).ToList();
+            
+            int ResultStat = maps.Count * 100 / AllMapsTeam.Count;
             if (time == 0 && kills == 0)
             {
                 maps = _myDbContext.Maps.Where(m => m.Game.Teamid == idTeam).ToList();
-                flagNull = true;
+                ResultStat = 0;
             }
             foreach (Map m in maps)
             {
@@ -62,17 +63,15 @@ namespace BetsMvc.Controllers
             }
             if (NumberGame != 0)
             {
-                var m = games.ElementAt(1).Maps;
                 games = games.Where(g => g.Maps.Count >= NumberGame).ToList();
                 ResultNumberGame = games.Count * 100 / Allgames.Count;
 
             }
-            int ResultStat = maps.Count * 100 / AllMapsTeam.Count;
-             
+
             TeamGamesModelView model = new TeamGamesModelView() {
                 Games = games, 
                 Maps = maps,
-                ResultStatus= ResultStat,
+                ResultStatus= ResultStat>=100 ? 100:ResultStat,
                 ResultStatusNumber = ResultNumberGame == 100 ? 0 : ResultNumberGame
             };
             ViewData["IdTeam"] = idTeam;
